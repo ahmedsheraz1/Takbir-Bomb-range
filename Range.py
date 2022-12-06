da84546a9a1e7d48735a3dc2dd5c1754949cb376
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-06 13:52:47 +0500

    Create Range.py

---
+++ Range.py
@@ -0,0 +1,16 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+for x in range(1, 11):
+    print('{0:2d} {1:3d} '.format(x, x*x ))
+
+    
+ 1   1 
+ 2   4 
+ 3   9 
+ 4  16 
+ 5  25 
+ 6  36 
+ 7  49 
+ 8  64 
+ 9  81 
+10 100 
